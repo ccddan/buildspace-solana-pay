584026a1db0ab98a9b30232c8651144297ccb79c
ccddan <ccdd.dev@gmail.com>
2022-06-02 23:14:28 -0500

    fix: use dev wallet for seller address

--- pages/api/createTransaction.ts
+++ pages/api/createTransaction.ts
@@ -17,7 +17,7 @@ import products from './products.json'
 const usdcAddress = new PublicKey(
   'Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr'
 )
-const sellerAddress = 'B1aLAAe4vW8nSQCetXnYqJfRxzTjnbooczwkUJAr7yMS'
+const sellerAddress = 'ZDn2SL19B66HC5RPuLJZXrP423XjTa62K1U7ohoLL4N'
 const sellerPublicKey = new PublicKey(sellerAddress)
 
 const createTransaction = async (req: any, res: any) => {
