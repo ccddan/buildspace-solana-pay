e84424a9069f76bcd7e6d65913e6d5c559af29f0
ccddan <ccdd.dev@gmail.com>
2022-06-02 19:20:15 -0500

    feat: fetch products from api

--- components/Products/ProductsCollection.tsx
+++ components/Products/ProductsCollection.tsx
@@ -20,6 +20,9 @@ export const ProductsCollection = (props: ProductsCollectionProps) => {
           </h2>
         </div>
 
+        {!products.length && (
+          <p>This category does not have products for sale.</p>
+        )}
         <div className="grid grid-cols-1 mt-8 md:grid-cols-2 lg:grid-cols-4 gap-x-4 gap-y-8">
           {products.map((product: Product, idx: number) => (
             <ProductCard key={idx} {...product} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,31 @@
+import { useConnection, useWallet } from '@solana/wallet-adapter-react'
+import { useEffect, useState } from 'react'
+
 import Head from '../components/Head'
 import IPFSDownload from '../components/IpfsDownload'
 import Navbar from '../components/Navbar'
 import type { NextPage } from 'next'
+import { Product } from '@models/product'
 import { ProductsCollection } from '../components/Products'
-const products = require('./api/products.json')
 
 const Home: NextPage = () => {
+  const { publicKey, connected } = useWallet()
+  const { connection } = useConnection()
+  const [products, setProducts] = useState<Product[]>([])
+
+  useEffect(() => {
+    if (connected && publicKey) {
+      fetch('/api/fetchProducts')
+        .then((response) => response.json())
+        .then((data) => setProducts(data))
+        .catch((err) => console.error(`Failed to fetch products:`, err))
+    }
+
+    if (!connected) {
+      setProducts([])
+    }
+  }, [publicKey, connected])
+
   return (
     <>
       <Head />
