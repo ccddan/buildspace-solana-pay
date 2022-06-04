3a40580a1d07781470d4d212533725eb6286bfbe
ccddan <ccdd.dev@gmail.com>
2022-06-03 23:04:01 -0500

    feat: dynamically add new products directly from webapp

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 
+import { CreateProduct } from '../components/Products'
 import Head from '../components/Head'
 import Navbar from '../components/Navbar'
 import type { NextPage } from 'next'
@@ -9,14 +10,22 @@ import { useWallet } from '@solana/wallet-adapter-react'
 
 const Home: NextPage = () => {
   const { publicKey, connected } = useWallet()
+  const isOwner = publicKey
+    ? publicKey.toString() === process.env.NEXT_PUBLIC_OWNER_PUBLIC_KEY
+    : false
+  const [creating, setCreating] = useState(false)
   const [products, setProducts] = useState<Product[]>([])
+
+  const fetchProducts = () => {
+    fetch('/api/fetchProducts')
+      .then((response) => response.json())
+      .then((data) => setProducts(data))
+      .catch((err) => console.error(`Failed to fetch products:`, err))
+  }
 
   useEffect(() => {
     if (connected && publicKey) {
-      fetch('/api/fetchProducts')
-        .then((response) => response.json())
-        .then((data) => setProducts(data))
-        .catch((err) => console.error(`Failed to fetch products:`, err))
+      fetchProducts()
     }
 
     if (!connected) {
@@ -30,6 +39,15 @@ const Home: NextPage = () => {
       <div className="bg-gray-900 text-slate-200">
         <Navbar />
         <div className="container m-auto">
+          {isOwner && (
+            <button
+              className="block m-auto px-3 py-3 text-sm font-medium text-white transition bg-lime-600 rounded-lg hover:bg-lime-700 focus:outline-none focus:ring"
+              onClick={() => setCreating(!creating)}
+            >
+              {creating ? 'Close' : 'Create Product'}
+            </button>
+          )}
+          {creating && <CreateProduct onProductCreated={fetchProducts} />}
           {!connected && (
             <p className="text-center">
               Connect with you wallet to see all available products
